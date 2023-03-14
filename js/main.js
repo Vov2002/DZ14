dd519ed9b657e351f116e4bac231d915d6d03f10
Vov2002 <Vladimiy2002@mail.ru>
2023-03-14 21:58:20 +0300

    DZ14

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//1 вариант
+// fetch('http://127.0.0.1:5500/index.html')
+// .then(data =>{
+//     console.log(data)
+//     data.text().then(data2 =>{
+//         console.log(data2)
+//     })
+// })
+//2 вариант
+// fetch('http://127.0.0.1:5500/index.html')
+// .then(data =>{
+// console.log(data)
+// return data.text()
+// })
+// .then(data =>{
+//     console.log(data)
+//     })
+
+//3 вариант
+// fetch('http://127.0.0.1:5500/index.html')
+// .then(data => data.text())
+// .then(data => console.log(data))
+let a = new Promise((resolve, reject) =>{
+    fetch('http://127.0.0.1:5500/index.html')
+    .then(data =>{
+    resolve(data.text())
+    })
+})
+    let b = new Promise((resolve, reject) =>{
+        fetch('http://127.0.0.1:5500/index.html')
+        .then(data =>{
+        resolve(data.text())
+        })
+    })
+    // a.then(data => {
+    // console.log(data)
+    // })
+
+    Promise.all([a,b]).then(data =>{
+        console.log(data)
+    })
